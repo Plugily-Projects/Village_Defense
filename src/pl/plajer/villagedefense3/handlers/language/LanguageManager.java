5fb892d90f68e8db83a74190c5ea235525b66354
Plajer <worthplay.pt@onet.pl>
2018-06-29 14:09:12 +0200

    Locale suggester

--- src/pl/plajer/villagedefense3/handlers/language/LanguageManager.java
+++ src/pl/plajer/villagedefense3/handlers/language/LanguageManager.java
@@ -18,6 +18,9 @@
 
 package pl.plajer.villagedefense3.handlers.language;
 
+import com.earth2me.essentials.Essentials;
+import com.wasteofplastic.askyblock.ASLocale;
+import com.wasteofplastic.askyblock.ASkyBlock;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -43,6 +46,10 @@ public class LanguageManager {
             plugin.saveResource("language.yml", false);
         }
         setupLocale();
+        //we will wait until server is loaded, we won't soft depend those plugins
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            if(pluginLocale == Locale.ENGLISH) suggestLocale();
+        }, 100);
     }
 
     private static void loadProperties() {
@@ -56,7 +63,7 @@ public class LanguageManager {
 
     private static void setupLocale() {
         String locale = plugin.getConfig().getString("locale", "default");
-        switch(locale.toLowerCase()){
+        switch(locale.toLowerCase()) {
             case "default":
             case "english":
             case "en":
@@ -102,6 +109,48 @@ public class LanguageManager {
         loadProperties();
     }
 
+    private static void suggestLocale() {
+        //we will catch any exceptions in case of api changes
+        boolean hasLocale = false;
+        String localeName = "";
+        try {
+            if(plugin.getServer().getPluginManager().isPluginEnabled("ASkyBlock")) {
+                ASLocale locale = ASkyBlock.getPlugin().myLocale();
+                switch(locale.getLocaleName()) {
+                    case "pl-PL":
+                    case "de-DE":
+                    case "es-ES":
+                    case "fr-FR":
+                    case "vn-VN":
+                        hasLocale = true;
+                        localeName = locale.getLocaleName();
+                }
+            }
+            if(plugin.getServer().getPluginManager().isPluginEnabled("Essentials")) {
+                Essentials ess = (Essentials) plugin.getServer().getPluginManager().getPlugin("Essentials");
+                java.util.Locale locale = ess.getI18n().getCurrentLocale();
+                switch(locale.getCountry()) {
+                    case "PL":
+                    case "DE":
+                    case "ES":
+                    case "FR":
+                    case "ID":
+                    case "VN":
+                        hasLocale = true;
+                        localeName = locale.getDisplayName();
+                }
+            }
+        } catch(Exception e) {
+            Main.debug("[WARN] Plugin has occured a problem suggesting locale, probably API change.", System.currentTimeMillis());
+        }
+        if(hasLocale) {
+            MessageUtils.info();
+            Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "[Village Defense] We've found that you use locale " + localeName + " in other plugins.");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "We recommend you to change plugin's locale to " + localeName + " to have best plugin experience.");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "You can change plugin's locale in config.yml in locale section!");
+        }
+    }
+
     //todo do something with me
     public static FileConfiguration getLanguageFile() {
         return ConfigurationManager.getConfig("language");
@@ -122,7 +171,7 @@ public class LanguageManager {
         if(pluginLocale != Locale.ENGLISH) {
             try {
                 return properties.getProperty(ChatColor.translateAlternateColorCodes('&', message));
-            } catch(NullPointerException ex){
+            } catch(NullPointerException ex) {
                 MessageUtils.errorOccured();
                 Bukkit.getConsoleSender().sendMessage("Game message not found!");
                 Bukkit.getConsoleSender().sendMessage("Please regenerate your language.yml file! If error still occurs report it to the developer!");
