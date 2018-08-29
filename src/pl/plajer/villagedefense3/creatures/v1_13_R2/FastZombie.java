18585622b9f4c614599d8ef180427f6ac1f87c5b
Plajer <worthplay.pt@onet.pl>
2018-08-29 18:48:47 +0200

    1.13.1 support

---
+++ src/pl/plajer/villagedefense3/creatures/v1_13_R2/FastZombie.java
@@ -0,0 +1,95 @@
+/*
+ * Village Defense 3 - Protect villagers from hordes of zombies
+ * Copyright (C) 2018  Plajer's Lair - maintained by Plajer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.plajer.villagedefense3.creatures.v1_13_R2;
+
+import java.util.LinkedHashSet;
+
+import net.minecraft.server.v1_13_R2.EntityHuman;
+import net.minecraft.server.v1_13_R2.EntityIronGolem;
+import net.minecraft.server.v1_13_R2.EntityVillager;
+import net.minecraft.server.v1_13_R2.GenericAttributes;
+import net.minecraft.server.v1_13_R2.Navigation;
+import net.minecraft.server.v1_13_R2.PathfinderGoalFloat;
+import net.minecraft.server.v1_13_R2.PathfinderGoalHurtByTarget;
+import net.minecraft.server.v1_13_R2.PathfinderGoalLookAtPlayer;
+import net.minecraft.server.v1_13_R2.PathfinderGoalMoveTowardsRestriction;
+import net.minecraft.server.v1_13_R2.PathfinderGoalNearestAttackableTarget;
+import net.minecraft.server.v1_13_R2.PathfinderGoalRandomLookaround;
+import net.minecraft.server.v1_13_R2.PathfinderGoalSelector;
+import net.minecraft.server.v1_13_R2.PathfinderGoalZombieAttack;
+import net.minecraft.server.v1_13_R2.World;
+import net.minecraft.server.v1_13_R2.EntityZombie;
+
+import org.bukkit.craftbukkit.v1_13_R2.CraftWorld;
+
+import pl.plajer.villagedefense3.Main;
+import pl.plajer.villagedefense3.creatures.CreatureUtils;
+
+/**
+ * Created by Tom on 14/08/2014.
+ */
+public class FastZombie extends EntityZombie {
+
+  public int damage;
+  private float bw;
+
+  public FastZombie(org.bukkit.World world) {
+    this(((CraftWorld) world).getHandle());
+  }
+
+  @SuppressWarnings("rawtypes")
+  public FastZombie(World world) {
+    super(world);
+    this.bw = Main.ZOMBIE_SPEED; //Change this to your liking. this is were you set the speed
+    this.damage = 15; // set the damage
+    //There's also a ton of options of you do this. play around with it
+
+
+    LinkedHashSet goalB = (LinkedHashSet) CreatureUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
+    goalB.clear();
+    LinkedHashSet goalC = (LinkedHashSet) CreatureUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+    goalC.clear();
+    LinkedHashSet targetB = (LinkedHashSet) CreatureUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
+    targetB.clear();
+    LinkedHashSet targetC = (LinkedHashSet) CreatureUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+    targetC.clear();
+
+
+    ((Navigation) getNavigation()).b(true);
+
+    this.goalSelector.a(0, new PathfinderGoalFloat(this));
+    this.goalSelector.a(2, new PathfinderGoalZombieAttack(this, this.bw, false));
+    this.goalSelector.a(4, new PathfinderGoalMoveTowardsRestriction(this, this.bw));
+    this.goalSelector.a(7, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F)); // this one to look at human
+    this.goalSelector.a(7, new PathfinderGoalRandomLookaround(this));
+    this.targetSelector.a(1, new PathfinderGoalHurtByTarget(this, true));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityHuman.class, true)); // this one to target human
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityVillager.class, false));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityIronGolem.class, false));
+    this.p(true);
+  }
+
+  @Override
+  protected void initAttributes() {
+    super.initAttributes();
+    this.getAttributeInstance(GenericAttributes.FOLLOW_RANGE).setValue(100.0D);
+  }
+
+
+}
