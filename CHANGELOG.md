554c22b0e0a8af5437a0c234be65d6bdd09c9cf0
Plajer <worthplay.pt@onet.pl>
2018-09-07 15:25:29 +0200

    Force stop arenas to prevent problems

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ### 3.12 Release (07.09.2018)
 * Dropped 1.9-1.10 support
+* /vda reload now force players to quit to prevent problems
 
 ### 3.11.2 Release (03.09.2018)
 * Temporarily merged PLCore to fix issues when using my other plugins
--- src/main/java/pl/plajer/villagedefense3/commands/AdminCommands.java
+++ src/main/java/pl/plajer/villagedefense3/commands/AdminCommands.java
@@ -253,6 +253,9 @@ public class AdminCommands extends MainCommand {
     if (!hasPermission(sender, "villagedefense.admin.reload")) {
       return;
     }
+    for (Arena arena : ArenaRegistry.getArenas()) {
+      ArenaManager.stopGame(true, arena);
+    }
     ArenaRegistry.registerArenas();
     sender.sendMessage(ChatManager.PLUGIN_PREFIX + ChatManager.colorMessage("Commands.Admin-Commands.Success-Reload"));
   }
