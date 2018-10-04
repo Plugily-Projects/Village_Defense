51a843275fab283b63d0e11538551d446b874c4a
Plajer <worthplay.pt@onet.pl>
2018-10-04 15:36:17 +0200

    Copyright update

--- src/main/java/pl/plajer/villagedefense4/api/event/player/package-info.java
+++ src/main/java/pl/plajer/villagedefense4/api/event/player/package-info.java
@@ -1,6 +1,6 @@
 /*
- * Village Defense 3 - Protect villagers from hordes of zombies
- * Copyright (C) 2018  Plajer's Lair - maintained by Plajer
+ * Village Defense 4 - Protect villagers from hordes of zombies
+ * Copyright (C) 2018  Plajer's Lair - maintained by Plajer and Tigerpanzer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
