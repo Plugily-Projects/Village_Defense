a59f0ad3c6a275d1567fe9935fe462f5904b5b97
Plajer <plajer.xyz@gmail.com>
2019-04-29 17:21:05 +0200

    Testing 1.14 version support

---
+++ src/main/java/pl/plajer/villagedefense/creatures/v1_14_R1/BabyZombie.java
@@ -0,0 +1,86 @@
+/*
+ * Village Defense - Protect villagers from hordes of zombies
+ * Copyright (C) 2019  Plajer's Lair - maintained by Plajer and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.plajer.villagedefense.creatures.v1_14_R1;
+
+import java.util.LinkedHashSet;
+
+import net.minecraft.server.v1_14_R1.EntityHuman;
+import net.minecraft.server.v1_14_R1.EntityIronGolem;
+import net.minecraft.server.v1_14_R1.EntityVillager;
+import net.minecraft.server.v1_14_R1.EntityZombie;
+import net.minecraft.server.v1_14_R1.GenericAttributes;
+import net.minecraft.server.v1_14_R1.PathfinderGoalBreakDoor;
+import net.minecraft.server.v1_14_R1.PathfinderGoalFloat;
+import net.minecraft.server.v1_14_R1.PathfinderGoalHurtByTarget;
+import net.minecraft.server.v1_14_R1.PathfinderGoalLookAtPlayer;
+import net.minecraft.server.v1_14_R1.PathfinderGoalMoveTowardsRestriction;
+import net.minecraft.server.v1_14_R1.PathfinderGoalNearestAttackableTarget;
+import net.minecraft.server.v1_14_R1.PathfinderGoalRandomLookaround;
+import net.minecraft.server.v1_14_R1.PathfinderGoalSelector;
+import net.minecraft.server.v1_14_R1.PathfinderGoalZombieAttack;
+import net.minecraft.server.v1_14_R1.World;
+
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+
+import pl.plajer.villagedefense.creatures.CreatureUtils;
+
+/**
+ * Created by Tom on 14/08/2014.
+ */
+public class BabyZombie extends EntityZombie {
+
+  public BabyZombie(org.bukkit.World world) {
+    this(((CraftWorld) world).getHandle());
+  }
+
+  public BabyZombie(World world) {
+    super(world);
+
+    LinkedHashSet goalB = (LinkedHashSet) CreatureUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
+    goalB.clear();
+    LinkedHashSet goalC = (LinkedHashSet) CreatureUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+    goalC.clear();
+    LinkedHashSet targetB = (LinkedHashSet) CreatureUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
+    targetB.clear();
+    LinkedHashSet targetC = (LinkedHashSet) CreatureUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+    targetC.clear();
+
+    getNavigation().q().b(true);
+
+    this.goalSelector.a(0, new PathfinderGoalFloat(this));
+    this.goalSelector.a(1, new PathfinderGoalBreakDoor(this, enumDifficulty -> true));
+    this.goalSelector.a(2, new PathfinderGoalZombieAttack(this, CreatureUtils.getBabyZombieSpeed(), false));
+    this.goalSelector.a(4, new PathfinderGoalMoveTowardsRestriction(this, CreatureUtils.getBabyZombieSpeed()));
+    this.goalSelector.a(7, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F)); // this one to look at human
+    this.goalSelector.a(7, new PathfinderGoalRandomLookaround(this));
+    this.targetSelector.a(1, new PathfinderGoalHurtByTarget(this));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityHuman.class, true)); // this one to target human
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityVillager.class, false));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityIronGolem.class, false));
+    this.setBaby(true);
+    this.setHealth(2);
+    this.p(true);
+  }
+
+  @Override
+  protected void initAttributes() {
+    super.initAttributes();
+    this.getAttributeInstance(GenericAttributes.FOLLOW_RANGE).setValue(200.0D);
+  }
+}
---
+++ src/main/java/pl/plajer/villagedefense/creatures/v1_14_R1/VillagerSlayer.java
@@ -0,0 +1,81 @@
+/*
+ * Village Defense - Protect villagers from hordes of zombies
+ * Copyright (C) 2019  Plajer's Lair - maintained by Plajer and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.plajer.villagedefense.creatures.v1_14_R1;
+
+import java.util.LinkedHashSet;
+
+import net.minecraft.server.v1_14_R1.EntityVillager;
+import net.minecraft.server.v1_14_R1.EntityZombie;
+import net.minecraft.server.v1_14_R1.GenericAttributes;
+import net.minecraft.server.v1_14_R1.PathfinderGoalBreakDoor;
+import net.minecraft.server.v1_14_R1.PathfinderGoalFloat;
+import net.minecraft.server.v1_14_R1.PathfinderGoalMeleeAttack;
+import net.minecraft.server.v1_14_R1.PathfinderGoalMoveTowardsRestriction;
+import net.minecraft.server.v1_14_R1.PathfinderGoalNearestAttackableTarget;
+import net.minecraft.server.v1_14_R1.PathfinderGoalRandomLookaround;
+import net.minecraft.server.v1_14_R1.PathfinderGoalSelector;
+import net.minecraft.server.v1_14_R1.World;
+
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+
+import pl.plajer.villagedefense.creatures.CreatureUtils;
+
+/**
+ * @author Plajer
+ * <p>
+ * Created at 02.05.2018
+ */
+public class VillagerSlayer extends EntityZombie {
+
+  public VillagerSlayer(org.bukkit.World world) {
+    this(((CraftWorld) world).getHandle());
+  }
+
+  public VillagerSlayer(World world) {
+    super(world);
+
+    LinkedHashSet goalB = (LinkedHashSet) CreatureUtils.getPrivateField("b", PathfinderGoalSelector.class, goalSelector);
+    goalB.clear();
+    LinkedHashSet goalC = (LinkedHashSet) CreatureUtils.getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+    goalC.clear();
+    LinkedHashSet targetB = (LinkedHashSet) CreatureUtils.getPrivateField("b", PathfinderGoalSelector.class, targetSelector);
+    targetB.clear();
+    LinkedHashSet targetC = (LinkedHashSet) CreatureUtils.getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+    targetC.clear();
+
+    getNavigation().q().b(true);
+
+    this.goalSelector.a(0, new PathfinderGoalFloat(this));
+    this.goalSelector.a(1, new PathfinderGoalBreakDoor(this, enumDifficulty -> true));
+    this.goalSelector.a(2, new PathfinderGoalMeleeAttack(this, 1.0f, false));
+    this.goalSelector.a(4, new PathfinderGoalMoveTowardsRestriction(this, 1.0f));
+    this.goalSelector.a(7, new PathfinderGoalRandomLookaround(this));
+    this.targetSelector.a(2, new PathfinderGoalNearestAttackableTarget<>(this, EntityVillager.class, true));
+    this.setHealth(70);
+    this.p(true);
+  }
+
+  @Override
+  protected void initAttributes() {
+    super.initAttributes();
+    this.getAttributeInstance(GenericAttributes.FOLLOW_RANGE).setValue(200.0D);
+    this.getAttributeInstance(GenericAttributes.KNOCKBACK_RESISTANCE).setValue(0D);
+  }
+
+}
