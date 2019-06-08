b69cdf270bd31b4a0d32331fe1b5d2d8ef3d28e0
Plajer <plajer.xyz@gmail.com>
2019-06-08 14:17:37 +0200

    Implement Constants tests

---
+++ src/test/java/pl/plajer/villagedefense/utils/ConstantsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Village Defense - Protect villagers from hordes of zombies
+ * Copyright (C) 2019  Plajer's Lair - maintained by Plajer and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.plajer.villagedefense.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Plajer
+ * <p>
+ * Created at 08.06.2019
+ */
+public class ConstantsTest {
+
+  @Test
+  public void testAllConstants() {
+    Assert.assertEquals("arenas", Constants.Files.ARENAS.getName());
+    Assert.assertEquals("config", Constants.Files.CONFIG.getName());
+    Assert.assertEquals("language", Constants.Files.LANGUAGE.getName());
+    Assert.assertEquals("bungee", Constants.Files.BUNGEE.getName());
+    Assert.assertEquals("kits", Constants.Files.KITS.getName());
+    Assert.assertEquals("lobbyitems", Constants.Files.LOBBY_ITEMS.getName());
+    Assert.assertEquals("mysql", Constants.Files.MYSQL.getName());
+    Assert.assertEquals("rewards", Constants.Files.REWARDS.getName());
+    Assert.assertEquals("stats", Constants.Files.STATS.getName());
+  }
+
+}
