a298cde036e9f1c042ec378151b2eacb2e6a1a22
Plajer <plajer.xyz@gmail.com>
2019-07-14 13:10:27 +0200

    Stop sending Mysql errors

--- src/main/java/pl/plajer/villagedefense/utils/ExceptionLogHandler.java
+++ src/main/java/pl/plajer/villagedefense/utils/ExceptionLogHandler.java
@@ -37,7 +37,7 @@ public class ExceptionLogHandler extends Handler {
 
   //these classes if found in stacktraces won't be reported
   //to the Error Service
-  private List<String> blacklistedClasses = Arrays.asList("pl.plajer.villagedefense.user.data.MySQLManager", "pl.plajerlair.commonsbox.database.MySQLDatabase");
+  private List<String> blacklistedClasses = Arrays.asList("pl.plajer.villagedefense.user.data.MysqlManager", "pl.plajer.villagedefense.plajerlair.commonsbox.database.MysqlDatabase");
   private Main plugin;
 
   public ExceptionLogHandler(Main plugin) {
