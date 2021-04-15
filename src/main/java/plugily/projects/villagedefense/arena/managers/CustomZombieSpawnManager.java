978d47227e63b652e8fdde10ea760df9fdfae226
hsgamer <huynhqtienvtag@gmail.com>
2021-04-15 12:07:09 +0700

    indent

--- src/main/java/plugily/projects/villagedefense/arena/managers/CustomZombieSpawnManager.java
+++ src/main/java/plugily/projects/villagedefense/arena/managers/CustomZombieSpawnManager.java
@@ -5,12 +5,12 @@ import plugily.projects.villagedefense.arena.Arena;
 import java.util.Random;
 
 public interface CustomZombieSpawnManager {
-    /**
-     * Handle the zombie spawn
-     *
-     * @param random the random number generator
-     * @param arena the arena the manager is trying to spawn zombies
-     * @param spawn the amount to spawn
-     */
-    void spawnZombie(Random random, Arena arena, int spawn);
+  /**
+   * Handle the zombie spawn
+   *
+   * @param random the random number generator
+   * @param arena the arena the manager is trying to spawn zombies
+   * @param spawn the amount to spawn
+   */
+  void spawnZombie(Random random, Arena arena, int spawn);
 }
