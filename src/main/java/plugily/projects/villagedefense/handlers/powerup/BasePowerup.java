7e1b2b28766eeebf7738cfd1591183d957da2b51
Huynh Tien <huynhqtienvtag@gmail.com>
2021-05-02 21:29:19 +0700

    Extend the power-up registry to use the interface (#87)

---
+++ src/main/java/plugily/projects/villagedefense/handlers/powerup/BasePowerup.java
@@ -0,0 +1,45 @@
+package plugily.projects.villagedefense.handlers.powerup;
+
+import pl.plajerlair.commonsbox.minecraft.compat.xseries.XMaterial;
+
+import java.util.function.Consumer;
+
+/**
+ * The interface for power-ups
+ */
+public interface BasePowerup {
+    /**
+     * Get the id of the power-up
+     *
+     * @return the id
+     */
+    String getId();
+
+    /**
+     * Get the name of the power-up
+     *
+     * @return the name
+     */
+    String getName();
+
+    /**
+     * Get the description of the power-up
+     *
+     * @return the description
+     */
+    String getDescription();
+
+    /**
+     * Get the display material of the power-up
+     *
+     * @return the material
+     */
+    XMaterial getMaterial();
+
+    /**
+     * Get the pickup consumer for the power-up
+     *
+     * @return the pickup consumer
+     */
+    Consumer<PowerupPickupHandler> getOnPickup();
+}
