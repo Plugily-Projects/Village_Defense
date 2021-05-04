09dc476d1e50088ff45117ae167971fae0f29891
Huynh Tien <huynhqtienvtag@gmail.com>
2021-05-04 14:12:45 +0700

    Remove raw color codes in shop cost (#85)

--- src/main/java/plugily/projects/villagedefense/arena/managers/ShopManager.java
+++ src/main/java/plugily/projects/villagedefense/arena/managers/ShopManager.java
@@ -136,7 +136,7 @@ public class ShopManager {
       if(itemStack.hasItemMeta() && itemStack.getItemMeta().hasLore()) {
         for(String s : ComplementAccessor.getComplement().getLore(itemStack.getItemMeta())) {
           if(s.contains(plugin.getChatManager().colorMessage(Messages.SHOP_MESSAGES_CURRENCY_IN_SHOP)) || s.contains("orbs")) {
-            costString = ChatColor.stripColor(s).replaceAll("[^0-9]", "");
+            costString = ChatColor.stripColor(s).replaceAll("&[0-9a-zA-Z]", "").replaceAll("[^0-9]", "");
             break;
           }
         }
