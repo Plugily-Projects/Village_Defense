a07938f279c2e00e3e23048d78708a11f55afa31
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-05-30 17:51:44 +0200

    Fixed party (parties v3.1.0) not working anymore

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -26,6 +26,7 @@ That's all, matcher will stop when detects next line started with `###` match
 * Changed empty message is no longer sent
 * Added Thai and Turkish locale (Thanks to PoEditor Contributors)
 * Added configurable name visibility for Golem, Wolf and Villager
+* Fixed party (parties v3.1.0) not working anymore
 
 ### 4.5.9 (28.03.2021 - 13.04.2021)
 * Fix when Splash_potion material not existed on 1.8 servers
--- src/main/java/plugily/projects/villagedefense/handlers/party/PartiesPartyHandlerImpl.java
+++ src/main/java/plugily/projects/villagedefense/handlers/party/PartiesPartyHandlerImpl.java
@@ -39,7 +39,7 @@ public class PartiesPartyHandlerImpl implements PartyHandler {
     PartiesAPI api = Parties.getApi();
     PartyPlayer partyPlayer = api.getPartyPlayer(player.getUniqueId());
     if(partyPlayer == null) return false;
-    Party party = api.getParty(partyPlayer.getPartyName());
+    Party party = api.getParty(partyPlayer.getPartyId());
     return party != null && party.getMembers().size() > 1;
   }
 
@@ -47,7 +47,7 @@ public class PartiesPartyHandlerImpl implements PartyHandler {
   public GameParty getParty(Player player) {
     PartiesAPI api = Parties.getApi();
     PartyPlayer partyPlayer = api.getPartyPlayer(player.getUniqueId());
-    Party party = api.getParty(partyPlayer.getPartyName());
+    Party party = api.getParty(partyPlayer.getPartyId());
     return new GameParty(party.getOnlineMembers(true).stream().map(localPlayer -> Bukkit.getPlayer(localPlayer.getPlayerUUID())).collect(Collectors.toList()), Bukkit.getPlayer(party.getLeader()));
   }
 
