ea26d4342fe4177f842515cd6861b72040ea3465
Huynh Tien <huynhqtienvtag@gmail.com>
2021-06-14 20:38:05 +0700

    add GolemBusterSpawner & PlayerBusterSpawner

--- src/main/java/plugily/projects/villagedefense/arena/managers/ZombieSpawnManager.java
+++ src/main/java/plugily/projects/villagedefense/arena/managers/ZombieSpawnManager.java
@@ -126,56 +126,20 @@ public class ZombieSpawnManager {
       spawn = (int) Math.ceil(zombiesLimit / 2.0);
     }
 
-//    if(arena.getZombies().isEmpty()) {
-//      for(int i = 0; i <= spawn; i++) {
-//        if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-//          arena.spawnFastZombie(random);
-//        }
-//      }
-//    }
     arena.addOptionValue(ArenaOption.ZOMBIE_SPAWN_COUNTER, 1);
     if(arena.getOption(ArenaOption.ZOMBIE_SPAWN_COUNTER) == 20) {
       arena.setOptionValue(ArenaOption.ZOMBIE_SPAWN_COUNTER, 0);
     }
 
-//    if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) < 5 && arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-//      arena.spawnFastZombie(random);
-//      return;
-//    }
-
     if(arena.getOption(ArenaOption.ZOMBIE_SPAWN_COUNTER) == 5) {
-      if(random.nextInt(3) != 2) {
-//        for(int i = 0; i <= spawn; i++) {
-//          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-//            if(wave > 23) {
-//              if(random.nextInt(4) == 1) {
-//                arena.spawnVillagerSlayer(random);
-//              }
-//            } else if(wave > 20) {
-//              if(random.nextInt(3) == 1) {
-//                arena.spawnKnockbackResistantZombies(random);
-//              }
-//            } else if(wave > 14) {
-//              if(random.nextInt(2) == 1) {
-//                arena.spawnHardZombie(random);
-//              }
-//            } else if(wave > 7) {
-//              if (random.nextInt(2) == 1) {
-//                arena.spawnSoftHardZombie(random);
-//              }
-//            } else {
-//              arena.spawnFastZombie(random);
-//            }
-//          }
-//        }
-      } else {
+      if(random.nextInt(3) == 2) {
         for(int i = 0; i <= spawn; i++) {
-          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-            arena.spawnPlayerBuster(random);
-          }
-          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-            arena.spawnGolemBuster(random);
-          }
+//          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
+//            arena.spawnPlayerBuster(random);
+//          }
+//          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
+//            arena.spawnGolemBuster(random);
+//          }
           if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
             arena.spawnVillagerBuster(random);
           }
@@ -185,28 +149,13 @@ public class ZombieSpawnManager {
         }
       }
     }
-//    if(arena.getOption(ArenaOption.ZOMBIE_SPAWN_COUNTER) == 15 && wave > 4) {
-//      if(wave > 8) {
-//        for(int i = 0; i < (spawn - 7); i++) {
-//          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-//            arena.spawnHardZombie(random);
-//          }
-//        }
-//      } else {
-//        for(int i = 0; i < (spawn - 3); i++) {
-//          if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-//            arena.spawnSoftHardZombie(random);
-//          }
+//    if(random.nextInt(8) == 0 && wave > 10) {
+//      for(int i = 0; i < (spawn - 8); i++) {
+//        if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
+//          arena.spawnPlayerBuster(random);
 //        }
 //      }
 //    }
-    if(random.nextInt(8) == 0 && wave > 10) {
-      for(int i = 0; i < (spawn - 8); i++) {
-        if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-          arena.spawnPlayerBuster(random);
-        }
-      }
-    }
     if(random.nextInt(8) == 0 && wave > 7) {
       for(int i = 0; i < (spawn - 5); i++) {
         if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
@@ -226,13 +175,13 @@ public class ZombieSpawnManager {
         arena.spawnHalfInvisibleZombie(random);
       }
     }
-    if(random.nextInt(8) == 0 && !arena.getIronGolems().isEmpty() && wave >= 6) {
-      for(int i = 0; i < (spawn - 4); i++) {
-        if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
-          arena.spawnGolemBuster(random);
-        }
-      }
-    }
+//    if(random.nextInt(8) == 0 && !arena.getIronGolems().isEmpty() && wave >= 6) {
+//      for(int i = 0; i < (spawn - 4); i++) {
+//        if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
+//          arena.spawnGolemBuster(random);
+//        }
+//      }
+//    }
     if(random.nextInt(8) == 0 && !arena.getVillagers().isEmpty() && wave >= 15) {
       for(int i = 0; i < (spawn - 13); i++) {
         if(arena.getOption(ArenaOption.ZOMBIES_TO_SPAWN) > 0) {
---
+++ src/main/java/plugily/projects/villagedefense/arena/managers/spawner/GolemBusterSpawner.java
@@ -0,0 +1,56 @@
+package plugily.projects.villagedefense.arena.managers.spawner;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.ItemStack;
+import pl.plajerlair.commonsbox.minecraft.compat.VersionUtils;
+import pl.plajerlair.commonsbox.minecraft.compat.xseries.XMaterial;
+import plugily.projects.villagedefense.arena.Arena;
+import plugily.projects.villagedefense.creatures.CreatureUtils;
+
+public class GolemBusterSpawner implements SimpleZombieSpawner {
+    @Override
+    public int getMinWave() {
+        return 1;
+    }
+
+    @Override
+    public double getSpawnRate(Arena arena, int wave, int phase, int spawnAmount) {
+        if (phase == 5) {
+            return 1D / 3;
+        }
+        if (wave > 6 && !arena.getIronGolems().isEmpty()) {
+            return 1D / 8;
+        }
+        return 0;
+    }
+
+    @Override
+    public int getFinalAmount(Arena arena, int wave, int phase, int spawnAmount) {
+        if (phase == 5) {
+            return spawnAmount / 4;
+        }
+        if (wave > 6) {
+            return spawnAmount - 4;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean checkPhase(Arena arena, int wave, int phase, int spawnAmount) {
+        return phase == 5 || wave > 6;
+    }
+
+    @Override
+    public Zombie spawnZombie(Location location) {
+        Zombie golemBuster = CreatureUtils.getCreatureInitializer().spawnGolemBuster(location);
+        golemBuster.getEquipment().setHelmet(new ItemStack(Material.TNT));
+        golemBuster.getEquipment().setHelmetDropChance(0.0F);
+        VersionUtils.setItemInHandDropChance(golemBuster, 0F);
+        golemBuster.getEquipment().setBoots(XMaterial.IRON_BOOTS.parseItem());
+        golemBuster.getEquipment().setLeggings(XMaterial.IRON_LEGGINGS.parseItem());
+        golemBuster.getEquipment().setChestplate(XMaterial.IRON_CHESTPLATE.parseItem());
+        return golemBuster;
+    }
+}
