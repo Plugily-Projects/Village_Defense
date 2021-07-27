723f00cee2e469f3e5c289376df0b04816fa2f02
Huynh Tien <huynhqtienvtag@gmail.com>
2021-07-27 13:36:16 +0700

    ok

--- src/main/java/plugily/projects/villagedefense/kits/premium/CleanerKit.java
+++ src/main/java/plugily/projects/villagedefense/kits/premium/CleanerKit.java
@@ -107,13 +107,12 @@ public class CleanerKit extends PremiumKit implements Listener {
       e.getPlayer().sendMessage(getPlugin().getChatManager().colorMessage(Messages.KITS_CLEANER_NOTHING_TO_CLEAN));
       return;
     }
-    double maxHealth = getKitsConfig().getDouble("Kit-Settings.Cleaner.Max-Health-To-Clean", 2048);
     int amount = getKitsConfig().getInt("Kit-Settings.Cleaner.Base-Amount", 10);
     if (amount < arena.getEnemies().size()) {
       int increaseUnit = arena.getWave() / Math.max(1, getKitsConfig().getInt("Kit-Settings.Cleaner.Increase-After-Wave", 5));
       amount += increaseUnit * Math.max(0, getKitsConfig().getInt("Kit-Settings.Cleaner.Increase-Amount", 5));
     }
-    ArenaUtils.removeSpawnedEnemies(arena, amount, maxHealth);
+    ArenaUtils.removeSpawnedEnemies(arena, amount, getKitsConfig().getDouble("Kit-Settings.Cleaner.Max-Health-To-Clean", 2048));
 
     Utils.playSound(e.getPlayer().getLocation(), "ENTITY_ZOMBIE_DEATH", "ENTITY_ZOMBIE_DEATH");
     getPlugin().getChatManager().broadcastMessage(arena, getPlugin().getChatManager()
