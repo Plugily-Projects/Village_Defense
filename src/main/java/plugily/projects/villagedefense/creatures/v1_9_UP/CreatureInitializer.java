f62c71f8a941f605e39ce44f8a0a29d4aa83cd70
Plajer <plajer.xyz@gmail.com>
2023-08-28 14:14:23 +0200

    refactor: Minimum working version is now 1.12.2

--- src/main/java/plugily/projects/villagedefense/creatures/v1_9_UP/CreatureInitializer.java
+++ src/main/java/plugily/projects/villagedefense/creatures/v1_9_UP/CreatureInitializer.java
@@ -1,20 +1,20 @@
 
 /*
- *  Village Defense - Protect villagers from hordes of zombies
- *  Copyright (c) 2023 Plugily Projects - maintained by Tigerpanzer_02 and contributors
+ * Village Defense - Protect villagers from hordes of zombies
+ * Copyright (c) 2023  Plugily Projects - maintained by Tigerpanzer_02 and contributors
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package plugily.projects.villagedefense.creatures.v1_9_UP;
@@ -24,7 +24,6 @@ import org.bukkit.entity.Creature;
 import org.bukkit.entity.IronGolem;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.Wolf;
-import org.bukkit.entity.Zombie;
 import plugily.projects.villagedefense.creatures.BaseCreatureInitializer;
 import plugily.projects.villagedefense.creatures.CreatureUtils;
 
@@ -57,43 +56,4 @@ public class CreatureInitializer implements BaseCreatureInitializer {
     throw new ClassCastException("IronGolem creature isn't a iron golem");
   }
 
-  @Override
-  public Zombie spawnFastZombie(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnBabyZombie(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnHardZombie(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnPlayerBuster(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnGolemBuster(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnVillagerBuster(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnKnockbackResistantZombies(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
-
-  @Override
-  public Zombie spawnVillagerSlayer(Location location) {
-    throw new UnsupportedOperationException("Method isn't used on 1.9 and up");
-  }
 }
